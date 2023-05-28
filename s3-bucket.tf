e0adbbd770f33611672b62b638e52f55e5fa42fc
Naor <naor@terasky.com>
2023-05-28 15:06:55 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,12 +1,12 @@
 module "s3_bucket" {
   source = "terraform-aws-modules/s3-bucket/aws"
 
-  bucket = "my-s3-bucket"
+  bucket = "my-s3-bucket-02"
   acl    = "private"
 
   control_object_ownership = true
   object_ownership         = "ObjectWriter"
-  
+
   versioning = {
     enabled = true
   }
